8f90aa0b81eb9e5bebb0bd4d5c64b25e014c9b1d
Programming-Sai <saiahprog6@gmail.com>
2025-04-19 03:26:35 +0000

    Updated file paths for easy access externally

--- src/config/deviceConfigs.js
+++ src/config/deviceConfigs.js
@@ -1,24 +1,34 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Go UP directories to the project root (from src/config to project root)
+const projectRoot = path.resolve(__dirname, "../../"); // adjust if deeper
+
+
 const laptop = {
   name: "laptop",
-  mockupPath: "assets/Laptop.png",
+  mockupPath: path.resolve(projectRoot, "assets/Laptop.png"),
   screenPosition: { top: 142, left: 270, width: 1560, height: 980 },
   borderRadius: 1,
 };
 
 const tablet = {
   name: "tablet",
-  mockupPath: "assets/Tablet.png",
+  mockupPath: path.resolve(projectRoot, "assets/Tablet.png"),
   screenPosition: { top: 63, left: 82, width: 686, height: 985 },
   borderRadius: 20,
 };
 
 const mobile = {
   name: "mobile",
-  mockupPath: "assets/Mobile.png",
+  mockupPath: path.resolve(projectRoot, "assets/Mobile.png"),
   screenPosition: { top: 70, left: 30, width: 335, height: 670 },
   borderRadius: 20,
 };
 
-  
-  export { laptop, tablet, mobile };
-  
+console.log(mobile?.mockupPath)
+
+export { laptop, tablet, mobile };
